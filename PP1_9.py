194ef16ad08150324452609c4926aab554df9e3a
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-11-04 22:36:44 -0500

    Update PP1_9.py

--- PP1_9.py
+++ PP1_9.py
@@ -6,28 +6,28 @@
 '''
 
 def q1(): 
-  print ('"Hello World"')
+    print ('"Hello World"')
 
 
 def q2(): 
-  word = input("Input a word: ")
-  print (word.lower())
-  print (word.upper())
+    word = input("Input a word: ")
+    print (word.lower())
+    print (word.upper())
 
 
 def q3(): 
-  word5 = input("Input a word that is at least 5 letters long: ")
-  print(word5[1:4])
+    word5 = input("Input a word that is at least 5 letters long: ")
+    print(word5[1:4])
 
 
 def q4(): 
-  word = input("Input a word: ")
-  print(word.index ("o"))
+    word = input("Input a word: ")
+    print(word.index ("o"))
 
 
 def q5(): 
-  word = input("Input a word: ")
-  print (len(word))
+    word = input("Input a word: ")
+    print (len(word))
 
 
 
@@ -35,8 +35,8 @@ def q5():
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
-q2()
-q3()
-q4()
-q5()
+#q1()
+#q2()
+#q3()
+#q4()
+#q5()
