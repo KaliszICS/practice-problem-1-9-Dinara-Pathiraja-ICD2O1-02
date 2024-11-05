9d1947c69e07990bcd431f74787906032bce8a45
Dinara-Pathiraja-ICD2O1-02 <Dinara.pathiraja@student.tdsb.on.ca>
2024-11-04 22:33:51 -0500

    Update PP1_9.py

--- PP1_9.py
+++ PP1_9.py
@@ -1,23 +1,34 @@
-
+'''
+    Lesson: String Manipulation
+    Author: Dinara
+    Date Created: Sept 30, 2024
+    Date Last Modified: Sept 30, 2024
+'''
 
 def q1(): 
-  #Write Assignment code here
+  print ('"Hello World"')
 
 
 def q2(): 
-  #Write Assignment code here
+  word = input("Input a word: ")
+  print (word.lower())
+  print (word.upper())
 
 
 def q3(): 
-  #Write Assignment code here
+  word5 = input("Input a word that is at least 5 letters long: ")
+  print(word5[1:4])
 
 
 def q4(): 
-  #Write Assignment code here
+  word = input("Input a word: ")
+  print(word.index ("o"))
 
 
 def q5(): 
-  #Write Assignment code here
+  word = input("Input a word: ")
+  print (len(word))
+
 
 
 
